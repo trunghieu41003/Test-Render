99c794c0c48689ff58991d6de36133de020a9427
trunghieu41003 <146319700+trunghieu41003@users.noreply.github.com>
2024-09-21 19:36:57 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-const express = require('express')
-const app = express()
-const port = process.env
-const hostname= 'localhost'
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;  // Gán giá trị mặc định là 3000 nếu PORT không được thiết lập trong .env
+const hostname = 'localhost';
+require('dotenv').config();
 
-//khai báo route
+// Route mặc định cho URL /
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// Các route khác
 app.get('/abc', (req, res) => {
-  res.send('This is Hieu D')
-})
-
+  res.send('This is Hieu D');
+});
 
 app.get('/123', (req, res) => {
-  res.send('<h1>This is Hieu DDDD</h1>')
-})
+  res.send('<h1>This is Hieu DDDD</h1>');
+});
 
+// Lắng nghe server
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening at http://${hostname}:${port}`);
+});
